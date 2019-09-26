6a427cd8e7e1be61ea861a024dd552508ac2fc6a
Retrojb <baltescartography@gmail.com>
2019-09-26 16:40:12 -0400

    striping all of the generic styling

--- src/components/header.js
+++ src/components/header.js
@@ -4,20 +4,9 @@ import React from "react"
 import Navigation from "./navigation"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
+  <header>
+    <div>
+      <h1>
         {siteTitle}
       </h1>
       <Navigation />
